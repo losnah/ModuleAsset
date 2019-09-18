a20680ef87782a9beec2630d539b5911a2f4cf01
H S <gkskfhdlstm@naver.com>
2019-09-18 22:52:22 +0900

    Complete kakao Oauth module

--- KakaoLoginModule/app/src/main/java/com/onsemiro/hanpinetree/kakaologinmodule/GlobalApplication.java
+++ KakaoLoginModule/app/src/main/java/com/onsemiro/hanpinetree/kakaologinmodule/GlobalApplication.java
@@ -7,18 +7,22 @@ import com.kakao.auth.AuthType;
 import com.kakao.auth.IApplicationConfig;
 import com.kakao.auth.ISessionConfig;
 import com.kakao.auth.KakaoAdapter;
+import com.kakao.auth.KakaoSDK;
+
 public class GlobalApplication extends Application {
-    private static GlobalApplication instance;
-    public static final GlobalApplication getGlobalApplicationContext() {
-        if (instance == null)
+    private static volatile GlobalApplication instance = null;
+    public static GlobalApplication getGlobalApplicationContext() {
+        if(instance == null)
             throw new IllegalStateException("this application does not inherit com.kakao.GlobalApplication");
         return instance;
     }
-    protected static class KakaoSDKAdapter extends KakaoAdapter {
+
+
+
+    public static class KakaoSDKAdapter extends KakaoAdapter {
         /**
          * Session Config에 대해서는 default값들이 존재한다.
          * 필요한 상황에서만 override해서 사용하면 됨.
-         *
          * @return Session의 설정값.
          */
         @Override
@@ -26,26 +30,31 @@ public class GlobalApplication extends Application {
             return new ISessionConfig() {
                 @Override
                 public AuthType[] getAuthTypes() {
-                    return new AuthType[]{AuthType.KAKAO_LOGIN_ALL};
+                    return new AuthType[] {AuthType.KAKAO_LOGIN_ALL};
                 }
+
                 @Override
                 public boolean isUsingWebviewTimer() {
                     return false;
                 }
+
                 @Override
                 public boolean isSecureMode() {
                     return false;
                 }
+
                 @Override
                 public ApprovalType getApprovalType() {
                     return ApprovalType.INDIVIDUAL;
                 }
+
                 @Override
                 public boolean isSaveFormData() {
                     return true;
                 }
             };
         }
+
         @Override
         public IApplicationConfig getApplicationConfig() {
             return new IApplicationConfig() {
@@ -56,9 +65,11 @@ public class GlobalApplication extends Application {
             };
         }
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
+        KakaoSDK.init(new KakaoSDKAdapter());
     }
 }
--- KakaoLoginModule/app/src/main/java/com/onsemiro/hanpinetree/kakaologinmodule/MainActivity.java
+++ KakaoLoginModule/app/src/main/java/com/onsemiro/hanpinetree/kakaologinmodule/MainActivity.java
@@ -29,6 +29,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import static com.kakao.util.helper.Utility.getPackageInfo;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
@@ -40,6 +41,7 @@ import com.kakao.auth.KakaoSDK;
 import com.kakao.auth.Session;
 import com.kakao.util.exception.KakaoException;
 import com.kakao.util.helper.log.Logger;
+
 public class MainActivity extends Activity {
     private SessionCallback callback;
     /**
@@ -52,91 +54,17 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-//        KakaoSDK.init(new GlobalApplication.KakaoSDKAdapter());
-
-        if (KakaoSDK.getAdapter() == null) {
-            KakaoSDK.init(new  GlobalApplication.KakaoSDKAdapter());
-        }
         callback = new SessionCallback();
         Session.getCurrentSession().addCallback(callback);
         Session.getCurrentSession().checkAndImplicitOpen();
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
             Intent intent = new Intent(this, SuccessActivity.class);
             startActivity(intent);
-           // finish();
-            return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Session.getCurrentSession().removeCallback(callback);
-    }
-    private class SessionCallback implements ISessionCallback {
-        @Override
-        public void onSessionOpened() {
-            redirectSignupActivity();
-        }
-        @Override
-        public void onSessionOpenFailed(KakaoException exception) {
-            if (exception != null) {
-                Logger.e(exception);
-            }
-        }
-    }
-    protected void redirectSignupActivity() {
-        final Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
-}
-
-/*
-MainActivity.java
-package com.onsemiro.hanpinetree.kakaologinmodule;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-import com.kakao.auth.ISessionCallback;
-import com.kakao.auth.Session;
-import com.kakao.util.exception.KakaoException;
-import com.kakao.util.helper.log.Logger;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import static com.kakao.util.helper.Utility.getPackageInfo;
-
-public class MainActivity extends AppCompatActivity {
-    private SessionCallback callback;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        callback = new SessionCallback();
-        Session.getCurrentSession().addCallback(callback);
-        Session.getCurrentSession().checkAndImplicitOpen();
-        //Log.d("MyHashKey", getKeyHash(getApplicationContext()));
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            finish();
             return;
         }
 
@@ -165,27 +93,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void redirectSignupActivity() {
-        final Intent intent = new Intent(this, MainActivity.class);
+        final Intent intent = new Intent(this, SuccessActivity.class);
         startActivity(intent);
         finish();
     }
-    public static String getKeyHash(final Context context) {
-        PackageInfo packageInfo = getPackageInfo(context, PackageManager.GET_SIGNATURES);
-        if (packageInfo == null)
-            return null;
-
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
-            } catch (NoSuchAlgorithmException e) {
-                Log.w("태그", "Unable to get MessageDigest. signature=" + signature, e);
-            }
-        }
-        return null;
-    }
 }
-
-
- */
